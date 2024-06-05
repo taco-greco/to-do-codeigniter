d52dffebc777ba1e98719bf1e8fde282432c58a1
Anastasios Statiris <tasosstatiris@outlook.fr>
2024-06-05 16:07:25 +0200

    bootswatch sketchy

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -14,22 +14,22 @@
     <header class="m-3">
         <h1><a href="<?= url_to("Todos::index") ?>"><?= $this->renderSection('H1') ?></a></h1>
     </header>
-   
-        <main class="container">
+
+    <main class="container">
         <?php if (session()->getFlashdata('errors')) : ?>
 
-<div class="alert" role="alert">
-    <ul>
-        <?php foreach (session()->getFlashdata('errors') as $error) : ?>
-            <li><?= $error; ?></li>
-        <?php endforeach; ?>
-    </ul>
+            <div class="alert" role="alert">
+                <ul>
+                    <?php foreach (session()->getFlashdata('errors') as $error) : ?>
+                        <li><?= $error; ?></li>
+                    <?php endforeach; ?>
+                </ul>
 
-<?php endif; ?>
+            <?php endif; ?>
 
             <?= $this->renderSection('content') ?>
-        </main>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
