232fea5c0c0bd049b2f49eb51847f4399824d771
Anastasios Statiris <tasosstatiris@outlook.fr>
2024-06-20 14:01:53 +0200

    updated CI4 version

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -7,7 +7,7 @@
     <title><?= $this->renderSection('title') ?></title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.3/vapor/bootstrap.min.css" integrity="sha512-9LoG2EQdcmuEQpzkslqa3whcL8LM+7GGPsW2MK1gRlmiML0G7M5ZPj2aZaW2DyixslBfZoy4kANfP/MYWDCSiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+    <script src="https://unpkg.com/htmx.org@2.0.0"></script>
 </head>
 
 <body hx-boost="true">
